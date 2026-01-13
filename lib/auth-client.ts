fb819815ec0aa5144f8ef1dfd99176d502449895
azizurrbappy <azizurr.bappy@gmail.com>
2026-01-13 21:57:54 +0600

    add mongodb url in auth client file

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: 'mongodb+srv://mebappy:bappy1234@cluster0.4ekdt0p.mongodb.net/',
 });
