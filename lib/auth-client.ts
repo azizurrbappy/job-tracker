0c9d9b8444a6a48b988efcd03459031f16fb343b
azizurrbappy <azizurr.bappy@gmail.com>
2026-01-13 20:42:33 +0600

    added new base url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+const baseURL = process.env.BETTER_AUTH_URL
+    ? process.env.BETTER_AUTH_URL.startsWith("http")
+        ? process.env.BETTER_AUTH_URL
+        : `https://${process.env.BETTER_AUTH_URL}`
+    : "http://localhost:3000";
+
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL,
 });
